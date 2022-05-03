e902ada25d4f6f09eee5bf16e287b6cb3ec1043c
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-05-03 18:26:53 +0200

    Stage 4/5: Healing potion

--- web/app.py
+++ web/app.py
@@ -1,39 +1,62 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sys
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
+engine = create_engine("sqlite:///weather.db?check_same_thread=False")
 
+Base = declarative_base()
 weather_data = {
-    'BOSTON': {
+    'LONDON': {
         'degrees': 9,
         'state': 'Chilly',
-        'city': 'BOSTON'
+        'city': 'LONDON'
     },
-    'NEW YORK': {
+    'FAIRBANKS': {
         'degrees': 32,
         'state': 'Sunny',
-        'city': 'NEW YORK'
+        'city': 'FAIRBANKS'
     },
-    'EDMONTON': {
+    'IDAHO': {
         'degrees': -15,
         'state': 'Cold',
-        'city': 'EDMONTON'
+        'city': 'IDAHO'
     }
 }
 
 
+class City(Base):
+    __tablename__ = "city"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    rendered_cities = dict()
+    db_cities = session.query(City).all()
+    for city in db_cities:
+        rendered_cities.update({city.name: weather_data[city.name]})
+    return render_template('index.html', cities=rendered_cities)
 
 
 @app.route('/city', methods=['GET', 'POST'])
 def add_city():
     if request.method == 'POST':
         req = request.form
+
         if req['city_name']:
             city_name_ = str(req['city_name']).upper()
-            return render_template('index.html', city=weather_data[city_name_])
+            session.add(City(name=city_name_))
+            session.commit()
+    return redirect(url_for('index'))
 
 
 # don't change the following way to run flask:
