86bda79aa6149eb8249669a74df12347c65b3821
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-05-05 16:10:00 +0200

    Stage 5/5: Wrong city

--- web/app.py
+++ web/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+
+from flask import Flask, render_template, request, redirect, url_for, flash
 import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
+app.config.update(SECRET_KEY=os.urandom(24))
 engine = create_engine("sqlite:///weather.db?check_same_thread=False")
 
 Base = declarative_base()
@@ -43,8 +46,21 @@ def index():
     rendered_cities = dict()
     db_cities = session.query(City).all()
     for city in db_cities:
+        weather_data[city.name].update({"id": city.id})
         rendered_cities.update({city.name: weather_data[city.name]})
+        print(rendered_cities)
     return render_template('index.html', cities=rendered_cities)
+
+
+@app.route('/delete/<city_id>', methods=['GET', 'POST'])
+def delete(city_id):
+    city = session.query(City).filter_by(id=city_id).first()
+    if not city:
+        flash("The city doesn't exist!")
+    else:
+        session.delete(city)
+        session.commit()
+    return redirect('/')
 
 
 @app.route('/city', methods=['GET', 'POST'])
@@ -54,8 +70,15 @@ def add_city():
 
         if req['city_name']:
             city_name_ = str(req['city_name']).upper()
-            session.add(City(name=city_name_))
-            session.commit()
+            if city_name_ in weather_data:
+                if not session.query(City).filter_by(name=city_name_).first():
+                    session.add(City(name=city_name_))
+                    session.commit()
+                else:
+                    flash("The city has already been added to the list!")
+            else:
+                flash("The city doesn't exist!")
+
     return redirect(url_for('index'))
 
 
